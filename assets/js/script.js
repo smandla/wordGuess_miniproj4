ffa0cf325e38984d7839b16ed6d0fcdecbe582ce
darmstrong <armstrong2579@gmail.com>
2022-06-27 14:18:56 -0700

    start javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// create array of strings, choose randomly to pick word
+var words = ["javascript", "pet", "dinosaur", "toddler", "antacid"];
+
+// need to listen for button to click and start timer and choose word
+// using length of random string, generate blank spaces/ underscores
+var startButton = document.getElementById("start_button");
+
+var guessWordSection = document.getElementById("guess_word_section");
+
+startButton.addEventListener("click", playGame);
+
+function playGame() {
+    for (i=0; i < words[2].length; i++) {
+        console.log(words[2] [i]);
+        var letterDiv = document.createElement("div");
+        letterDiv.innerHTML = "_";
+        guessWordSection.appendChild(letterDiv);
+    }
+}
